4e87e32344d60a3dc49e25befa6363c69ae96fa0
SURFT!$ <mr.darck31@gmail.com>
2025-05-21 09:26:13 +0200

    Update setup file

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name="image-editor",
-    version="1.0.0",
+    name="image-editor",            # pip install image-editor
+    version="0.1.0",
     description="Modular image editor based on Pillow",
     author="Surft14",
-    packages=find_packages(),
+    url="https://github.com/Surft14/ImageEditor",
+    packages=find_packages(exclude=["tests*",]),
     install_requires=[
-        "Pillow>=8.0.0"
+        "Pillow>=8.0.0",
     ],
     python_requires=">=3.7",
-    entry_points={
-        "console_scripts": [
-            "imgedit=image_editor.main:main",
-        ],
-    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
 )
